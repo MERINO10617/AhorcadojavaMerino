07f3c25a2521cccef72ea904a58815ca7d1cb364
MERINO10617 <amerinos09@gmail.com>
2022-02-22 13:47:24 +0100

    version2

--- src/paquete1/Main.java
+++ src/paquete1/Main.java
@@ -3,5 +3,6 @@ package paquete1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Vamos a sincronizar con github");
+        System.out.println("Version2");
     }
 }
